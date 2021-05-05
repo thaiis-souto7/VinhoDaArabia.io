0da5a24ec40388aa6b3972ec471f4d375d4c98d9
Gustavo Marques <gugamello2014@yahoo.com>
2021-05-05 18:40:04 -0300

    Pacotes Slick Adicionados, estável

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-
 import './App.css';
+import Marcador from "./assets/marcador.png";
+import Almofada from "./assets/almofada.png";
+import Capsula from "./assets/capsula.png";
+import Copo from "./assets/copo.png";
+import Garrafa from "./assets/garrafa_térmica.png";
+import Grao from "./assets/grao_de_cafe.png";
+import Po from "./assets/po_de_cafe.png";
+import Xicara from "./assets/xicara.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function App() {
   return (
