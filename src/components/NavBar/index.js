afa605886bd04dc2bdae3fe17e9371b0023ac619
Lucashmcastro <lucashmcastro@outlook.com>
2021-05-05 21:45:31 -0300

    First Commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeaderCustom } from "./styles";
+import Grid from "@material-ui/core/Grid";
+//import Logo from "../../assets/logoPrincipal.png";
+
+export default function CenteredGrid() {
+  return (
+    <HeaderCustom container spacing={3}>
+      <Grid item xs={12}>
+        
+      </Grid>
+    </HeaderCustom>
+  );
+}
