855f7e29e6e1d7a518b6e92c0d2132a46e6759fb
Lucashmcastro <lucashmcastro@outlook.com>
2021-05-10 15:13:23 -0300

    Created rules

---
+++ src/slider.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Fade } from "react-slideshow-image";
+import s1 from "./assets/s1.jpg";
+import s2 from "./assets/s2.jpg";
+import s3 from "./assets/s3.jpg";
+import s4 from "./assets/s4.jpg";
+
+const CallbackExample = () => {
+  const [previousIndex, setPreviousIndex] = useState(null);
+  const [nextIndex, setNextIndex] = useState(null);
+
+  const style = {
+    textAlign: "center",
+    margin: "5rem 3rem 0rem 2rem ",
+    width: "100%",
+  };
+
+  const properties = {
+    autoplay: true,
+    indicators: true,
+    onChange: (previous, next) => {
+      setPreviousIndex(previous);
+      setNextIndex(next);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <Fade {...properties}>
+          <div style={{...style}}>
+            <img src={s1}></img>
+          </div>
+          <div style={{...style}}>
+            <img src={s2}></img>
+          </div>
+          <div style={{...style}}>
+          <img src={s3}></img>
+          </div>
+          <div style={{...style}}>
+          <img src={s4}></img>
+          </div>
+        </Fade>
+      </div>
+    </div>
+  );
+};
+
+export default CallbackExample;
