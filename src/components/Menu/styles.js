0d3ed86789c6dd31fc13d6b4ecfb6600867d0785
Lucashmcastro <lucashmcastro@outlook.com>
2021-05-11 10:19:54 -0300

    add Button

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
-
 export const Button = styled.button`
-background: #FFFFFF;
-border:1px solid #FFFFFF;
-color: #FFFFFF;
+
+color: rgba(193, 143, 29);
+margin: 1.5rem 3rem 0rem 3rem;
+font-size: 24px;
 
 `;
 
+export const MenuItem = styled.menuitem`
+
+// color: rgba(193, 143, 29);
+// padding: 3rem 3rem 0rem 3rem;
+// font-size: 24px;
+
+`;
